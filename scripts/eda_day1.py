3b3596d0e0542dd359c6190a78e21c505ba20834
aanito <howmarad@gmail.com>
2024-12-07 18:30:29 +0300

    Created the additional summary stats

--- scripts/eda_day1.py
+++ scripts/eda_day1.py
@@ -8,14 +8,16 @@ df = pd.read_csv(data_path)
 # Display the first few rows
 print(df.head())
 
-# Summary Statistics
-# print("Summary Statistics:")
-# print(df.describe())
 
-# Calculate additional metrics
-# numeric_cols = df.select_dtypes(include='number')  # Select numeric columns
-# for col in numeric_cols:
-#     print(f"\nColumn: {col}")
-#     print(f"Mean: {df[col].mean()}")
-#     print(f"Median: {df[col].median()}")
-#     print(f"Standard Deviation: {df[col].std()}")
+# Summary Statistics 
+numeric_cols = df.select_dtypes(include='number')  # Filtering only the numeric columns
+for col in numeric_cols:
+    print(f"\nColumn: {col}")
+    print(f"Mean: {df[col].mean()}")
+    print(f"Median: {df[col].median()}")
+    print(f"Standard Deviation: {df[col].std()}")
+
+
+# Other Summary Statistics
+print("Computed summary statistics:")
+print(df.describe())
